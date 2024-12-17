9af02778dad1261bc0ab560bcde61087bb5bd0a0
Joswen.Pinca <owenpinca2022@gmail.com>
2024-12-17 15:46:48 +0800

    Add files via upload

--- Pinca_Lab12.py
+++ Pinca_Lab12.py
@@ -1,8 +1,13 @@
+#While True code used to repeatedly ask for order/payments.
+
+#List of Food Items and Prices
 fooditems = ["Chicken Burger", "Cheeseburger", "Fries", "6 Pc Chicken Nuggets", "Coke Float", "Coca Cola", "Ice Tea", "Sprite"]
 foodprice = [60, 52, 35, 80, 50, 25, 25, 25]
+#List of ordered foods and their respective prices
 orderlist = []
 orderlistprice = []
 
+#Order System that uses indexing to get exact price for ordered food
 def OrderSystem():
     order=input("Choose between 1-8: ")
     while True:
@@ -20,6 +25,7 @@ def OrderSystem():
             print("Invalid Input. Please Try Again!")
         order=input("Order more? (Type Exit to Finish Order): ")
         
+#Payment System that displays change and if no change then another prompt is displayed. If insufficient cash then it allows user to try again. This is where program ends.
 def PaymentSystem():
     while True:
         payment = int(input("Input your Payment: "))
@@ -35,17 +41,26 @@ def PaymentSystem():
         else:
             print("Payment is Insufficient for your Order. Please Try Again!")
 
+#Main flow of code
 print("Welcome to MCDonalds! Here is our Menu:")
+
+#displays menu from the list with numbering and prices using index.
 for num in fooditems:
     index = fooditems.index(num)
     price = foodprice[index]
     print(f"{index+1}. {num}: {price}₱")
+
+#start of order process
 print("What will be your Order?")
 OrderSystem()
+
+#totalprice is outside of order process so that PaymentSystem function can use totalprice variable.
 totalprice = sum(orderlistprice)
 print(f"You have ordered:")
 for item in orderlist:
     print(item, end=' ')
     print()
 print(f"for {totalprice}₱.")
+
+#start of payment system
 PaymentSystem()
